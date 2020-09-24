bfbaa6195ec3f587414ad438f36873398fc9fd5c
shift_develope <53366878+shiftdevy@users.noreply.github.com>
2020-09-24 09:27:32 +0300

    Systems and plugins

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,36 @@
 use bevy::prelude::*;
 fn main() {
     App::build()
-        .add_system(hi.system())
+        .add_default_plugins()
+        .add_plugin(HelloPlugin)
         .run();
 }
 
 fn hi() {
     println!("Hi!");
+}
+
+struct Person;
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands
+        .spawn((Person, Name("First".to_string())))
+        .spawn((Person, Name("Second".to_string())))
+        .spawn((Person, Name("Third".to_string())));
+}
+
+fn greet_people(_person: &Person, name: &Name) {
+    println!("Hello, {}!", name.0);
+}
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        app
+            .add_startup_system(add_people.system())
+            .add_system(hi.system())
+            .add_system(greet_people.system());
+    }
 }
